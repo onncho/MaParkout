41c46972c7407fac3c706b0e877f20885c2c852c
onncho <onncho@users.noreply.github.com>
2020-05-15 01:00:27 +0300

    json

--- Apps/lib/data/workout.dart
+++ Apps/lib/data/workout.dart
@@ -1,6 +1,3 @@
-import 'package:flutter/cupertino.dart';
-import 'data.dart';
-
 //enum WorkoutType { TRX, Running, HITT }
 
 class Workout {
@@ -8,29 +5,42 @@ class Workout {
   String workoutName; // 2,
   //WorkoutType workoutType; //WorkoutType.HITT,
   String workoutCoach; // "Meital",
-  Icon workoutIcon; //"icon.svg",
+  String workoutIcon; //"icon.svg",
   bool workoutIsOnline; //false,
-  Location workoutLocation; //"some address",
+  String workoutLocation; //"some address",
   double workoutDurationTime; //1.5
+  String workoutTime; // "09:30",
+  String workoutColor; //"light blue"
 
   Workout(
-      this.workoutId,
+      {this.workoutId,
       this.workoutName,
       //this.workoutType,
       this.workoutCoach,
       this.workoutIcon,
       this.workoutDurationTime,
       this.workoutIsOnline,
-      this.workoutLocation);
+      this.workoutLocation,
+      this.workoutTime,
+      this.workoutColor});
 
-  Workout.fromJson(Map<String, dynamic> json) {
-    workoutId = json['workoutId'];
-    workoutName = json['workoutName'];
-    //workoutType = json['workoutType'];
-    workoutCoach = json['workoutCoach'];
-    workoutIcon = json['workoutIcon'];
-    workoutDurationTime = json['workoutDurationTime'];
-    workoutIsOnline = json['workoutIsOnline'];
-    workoutLocation = json['workoutLocation'];
+  @override
+  String toString() {
+    return '{ ${this.workoutId}, ${this.workoutCoach}, ${this.workoutDurationTime}, ${this.workoutIcon}, ${this.workoutIsOnline}, ${this.workoutLocation}, ${this.workoutName}, ${this.workoutTime}, ${this.workoutColor} }';
+  }
+
+  factory Workout.fromJson(Map<String, dynamic> json) {
+    return new Workout(
+      workoutId: json['workoutId'] as int,
+      workoutName: json['workoutName'] as String,
+      //workoutType = json['workoutType'];
+      workoutCoach: json['workoutCoach'] as String,
+      workoutIcon: json['workoutIcon'] as String,
+      workoutDurationTime: json['workoutDurationTime'] as double,
+      workoutIsOnline: json['workoutIsOnline'] as bool,
+      workoutLocation: json['workoutLocation'] as String,
+      workoutTime: json['workoutTime'] as String,
+      workoutColor: json['workoutColor'] as String,
+    );
   }
 }
--- Apps/lib/main.dart
+++ Apps/lib/main.dart
@@ -1,10 +1,12 @@
-import 'dart:js';
-
+import 'dart:convert';
 import 'package:MaParkOut/data/WorkoutState.dart';
+import 'package:MaParkOut/data/workout.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:splashscreen/splashscreen.dart';
 import 'package:MaParkOut/data/MyStateScheduler.dart';
+import 'package:http/http.dart' as http;
+import 'dart:async';
 
 void main() {
   runApp(
@@ -16,37 +18,104 @@ void main() {
         )
         // Provider(create: (context) => MyStateScheduler()),
       ],
-      child: MyApp(),
+      child: WorkoutApp(),
     ),
   );
 }
 
-class MyApp extends StatelessWidget {
+class WorkoutApp extends StatefulWidget {
+  @override
+  WorkoutAppState createState() => WorkoutAppState();
+}
+
+class WorkoutAppState extends State<WorkoutApp> {
   // This widget is the root of your application.
+
+  List<Workout> _allWorkouts = new List<Workout>();
+
+  Future<List<Workout>> fetchWorkouts() async {
+    var url =
+        'https://raw.githubusercontent.com/onncho/MaParkout/master/Apps/lib/data/workout.json';
+    var response = await http.get(url);
+
+    var workouts = List<Workout>();
+
+    if (response.statusCode == 200) {
+      Map<String, dynamic> myMap = json.decode(response.body);
+      List<dynamic> entitlements = myMap["workouts"];
+      workouts = entitlements.map((item) => Workout.fromJson(item)).toList();
+    }
+
+    return workouts;
+  }
+
+  @override
+  void initState() {
+    fetchWorkouts().then((value) {
+      setState(() {
+        _allWorkouts.addAll(value);
+      });
+    });
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'MA Parkout',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primarySwatch: Colors.yellow,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: new SplashScreen(
-          seconds: 6,
-          navigateAfterSeconds: new AfterSplash(),
-          title: new Text(
-            'Welcome In SplashScreen',
-            style: new TextStyle(fontWeight: FontWeight.bold, fontSize: 20.0),
-          ),
-          image: Image.asset('assets/ ma_repo/logo_on_white.jpeg'),
-          backgroundColor: Colors.white,
-          styleTextUnderTheLoader: new TextStyle(),
-          photoSize: 100.0,
-          onClick: () => print("user clicked"),
-          loaderColor: Colors.black),
-    );
+        title: 'MA Parkout',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          primarySwatch: Colors.yellow,
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+        ),
+        home: Center(
+          child: FutureBuilder(
+              future: fetchWorkouts(),
+              builder: (BuildContext context, AsyncSnapshot snapshot) {
+                if (snapshot.data == null) {
+                  return Container(child: CircularProgressIndicator());
+                }
+                if (snapshot.connectionState == ConnectionState.done) {
+                  orderData(snapshot.data);
+                  return new Scaffold(
+                      appBar: new AppBar(
+                          title: new Text("Welcome In SplashScreen Package"),
+                          automaticallyImplyLeading: false),
+                      body: new Center(
+                          child: ListView.builder(
+                              itemCount: snapshot.data.length,
+                              itemBuilder: (BuildContext context, int index) {
+                                return ListTile(
+                                    title:
+                                        Text(snapshot.data[index].workoutName));
+                              })));
+                } else if (snapshot.hasError) {
+                  return Text("${snapshot.error}");
+                }
+
+                // By default, show a loading spinner.
+                return CircularProgressIndicator();
+              }),
+        ));
+
+    //   new SplashScreen(
+    //       seconds: 6,
+    //       navigateAfterSeconds: new AfterSplash(),
+    //       title: new Text(
+    //         'Welcome In SplashScreen',
+    //         style: new TextStyle(fontWeight: FontWeight.bold, fontSize: 20.0),
+    //       ),
+    //       image: Image.asset('assets/ ma_repo/logo_on_white.jpeg'),
+    //       backgroundColor: Colors.white,
+    //       styleTextUnderTheLoader: new TextStyle(),
+    //       photoSize: 100.0,
+    //       onClick: () => print("user clicked"),
+    //       loaderColor: Colors.black),
+    // );
+  }
+
+  void orderData(json) {
+    var asd = 'askdnasd';
   }
 }
 
